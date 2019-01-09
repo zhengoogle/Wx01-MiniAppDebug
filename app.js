a22854a00cd1b87c39a401e78d1f082fe10b02ae
zhengoogle <zhengoogle@sina.com>
2019-01-09 23:52:44 +0800

    add tabBar & webview component;

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //app.js
 App({
   onLaunch: function () {
+		console.log('Version: v0.0.1')
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
---
+++ pages/view/advance/webview/WebView.js
@@ -0,0 +1,77 @@
+// pages/view/advance/webview/WebView.js
+/**
+ * wx view webview
+ * ref:
+ * ---https://developers.weixin.qq.com/miniprogram/dev/component/web-view.html
+ * trap:
+ * #1.打开网页失败，错误信息：不支持打开非业务域名
+ * ---测试版本支持设置不校验webview业务域名
+ * ---http://html51.com/info-44386-1/
+ * ---https://www.qinziheng.com/xiaochengxudev/4307.htm
+ * 
+ */
+Page({
+
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function(options) {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function() {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function() {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function() {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function() {
+
+	},
+
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+	onPullDownRefresh: function() {
+
+	},
+
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
+	onReachBottom: function() {
+
+	},
+
+	/**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function() {
+
+	}
+})
